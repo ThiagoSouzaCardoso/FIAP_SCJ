25f342d8cc7fcebac0bacccaeafdc9509cd14b7b
Thiago Souza Cardoso <kamui.thiago@gmail.com>
2016-04-20 22:31:11 -0300

    resolvido exercicio 2
    modificado forma de calculo

--- SCJ Fundamentos Aula 1/src/ex2/BinarioDecimal.java
+++ SCJ Fundamentos Aula 1/src/ex2/BinarioDecimal.java
@@ -1,28 +1,25 @@
 package ex2;
 public class BinarioDecimal {
 
-	public static void main(String[] args) {
-
-		int valor=12;
-		int quociente;
-		int resto;
-		String respost="";
-		
-		quociente=valor/2;
-		resto=valor%2;
-		System.out.println(resto);
-		respost=String.valueOf(resto)+respost;
-		
-		while (quociente>0){
-			resto=quociente%2;
-			quociente=quociente/2;
-			respost=String.valueOf(resto)+respost;
-			System.out.println(resto);
-		}
-
-		System.out.println(respost);
-		
-		 String bin = Integer.toString(valor, 2);  
-		 System.out.println(bin);
+    public String calculaBinario(int valor){
+	 int quociente = calculaQuociente(valor);
+	 int resto = calculaResto(valor);
+	 StringBuilder resposta = new StringBuilder(String.valueOf(resto));
+	 
+	 while(quociente>0){
+		 resto = calculaResto(quociente); 
+		 quociente = calculaQuociente(quociente);
+		 resposta = resposta.append(resto);
+	 }
+		return 	resposta.toString();
+    }
+	
+	
+	public int calculaQuociente(int valor){
+		return valor/2;
 	}
+	
+	public int calculaResto(int valor){
+		return valor%2;
+	
 }
